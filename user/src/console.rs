d5c62a2bf16767e4fa9a6fe5ae8443bbf0647c0f
alanvirus <alan3151717925@qq.com>
2024-07-17 23:48:26 +0800

    ch5

--- user/src/console.rs
+++ user/src/console.rs
@@ -1,9 +1,10 @@
-use super::write;
+use super::{write,read};
 use core::fmt::{self, Write};
 
 struct Stdout;
 
-const STDOUT: usize = 1;//fd==1 represents the std output
+const STDIN: usize = 0;
+const STDOUT: usize = 1; //fd==1 represents the std output
 
 impl Write for Stdout {
     fn write_str(&mut self, s: &str) -> fmt::Result {
@@ -28,4 +29,10 @@ macro_rules! println {
     ($fmt: literal $(, $($arg: tt)+)?) => {
         $crate::console::print(format_args!(concat!($fmt, "\n") $(, $($arg)+)?));
     }
+}
+
+pub fn getchar() -> u8 {
+    let mut c = [0u8; 1];
+    read(STDIN, &mut c);
+    c[0]
 }
